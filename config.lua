50ac774eb02b19e8965fe5c6e444e8f3f41344cd
Shadorossa <ignasingalcorrepersonal@gmail.com>
2025-09-04 21:48:01 +0200

    Updated config file

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 return {
     ["middle_blinds_abilities"] = true,
     ["winners_jokers"] = true,
-    ["oc_jokers"] = false,
+    ["oc_jokers"] = true,
     ["custom_middle_blinds"] = false,
+    ["no_oc_jokers"] = false,
     ["no_custom_middle_blinds"] = false,
     ["no_winners_jokers"] = false,
-    ["no_oc_jokers"] = false,
 }
